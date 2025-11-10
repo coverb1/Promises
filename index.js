c4e92b9bb1ec6c55ea478b6d1844c877ba0292c8
coverb1 <cboy85096@email.com>
2025-11-10 20:00:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function promise1() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("this is the first promise")
+        }, 2000);
+    })
+}
+
+function promise2() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("this is the first first")
+        }, 2000);
+    })
+}
+
+function promise3() {
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("this is the third first")
+        }, 2000);
+    })
+}
+Promise.race([promise1(), promise2(), promise3()])
+.then((message)=>{console.log(message)})
