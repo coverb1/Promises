f447e486d3bb8d6e7dd900b273a10ce664914b5a
coverb1 <cboy85096@email.com>
2025-11-11 16:49:43 +0200

    footercommit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function promise1() {
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-<<<<<<< HEAD
+
             resolve("this is the first promise")
-=======
+
             resolve("amanda")
->>>>>>> test-branch
+
         }, 2000);
     })
 }
 
 function promise2() {
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve("this is the first first")
         }, 2000);
@@ -19,11 +19,11 @@ function promise2() {
 }
 
 function promise3() {
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve("this is the third first")
         }, 2000);
     })
 }
 Promise.race([promise1(), promise2(), promise3()])
-.then((message)=>{console.log(message)})
+    .then((message) => { console.log(message) })
